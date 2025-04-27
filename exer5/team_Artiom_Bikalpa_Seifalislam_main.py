030ac40a0927ccb1b9f1a196d7a962d40338c8c5
Artiom <artiomartemii@gmail.com>
2025-04-27 16:26:02 +0200

    Created exer5 folder

---
+++ exer5/team_Artiom_Bikalpa_Seifalislam_main.py
@@ -0,0 +1,13 @@
+# -------------------   Developers   --------------------
+
+# Artiom Triboi        - artiom.triboi@stud.th-deg.de
+# Bikalpa Khachhibhoya - bikalpa.khachhibhoya@stud.th-deg.de
+# Seifalislam Sebak    - seifalislam.sebak@stud.th-deg.de
+
+
+import tkinter as tk
+import team_Artiom_Bikalpa_Seifalislam_view as view
+
+root = view.window("Library", "1200x720")
+mainWindow = view.mainView(root)
+root.mainloop()
